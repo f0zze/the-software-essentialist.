fd858336eded4a69ee0e2974fc1e1cead5f79f47
abeinarovics <a.beinarovics@onairent.live>
2023-03-20 20:07:06 +0200

    [New] should return 'Fizz for 3'

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,6 @@
 export function fizzBuzz(number: number) {
+    if (number % 3 === 0) {
+        return 'Fizz';
+    }
     return String(number);
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -3,5 +3,8 @@ describe("fizzbuzz", () => {
     it("should return a string", () => {
         expect(typeof fizzBuzz(1)).toBe("string");
     })
+    it("should return 'Fizz' for 3", () => {
+        expect(fizzBuzz(3)).toBe("Fizz");
+    })
 });
 
